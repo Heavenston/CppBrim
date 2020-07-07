ea3f203dde9f0ef0b9b1b4cf40b472bb63168828
Heavenstone <malolegendrelemaire@gmail.com>
2020-07-07 14:49:25 +0200

    added vm methods

--- vm/vm.cpp
+++ vm/vm.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 #include "brim.h"
 
-brim::VM::VM() {
+using namespace brim;
+
+VM::VM() {
     std::cout << "Created vm !\n";
 };
+
+/*
+void VM::interpret(char *program) {
+
+}
+void VM::interpret_file(char *path) {
+    
+}
+
+Chunk *VM::compile(char *program) {
+    
+}
+void VM::run() {
+    
+}*/
--- vm/vm.h
+++ vm/vm.h
@@ -1,10 +1,27 @@
 #pragma once
 
+#include <string>
+
+#include "chunk.h"
+#include "vec.h"
+#include "value.h"
+
 namespace brim {
 
     class VM {
+        brim::Vec<brim::Value> stack;
+        brim::Chunk *chunk = nullptr;
+        usize ip = 0;
+
     public:
         VM();
+
+        void interpret(char *program);
+        void interpret_file(char *path);
+
+        brim::Chunk *compile(char *program);
+        void run();
+
     };
 
 }
