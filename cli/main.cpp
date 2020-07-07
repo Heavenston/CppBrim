c771c0e162aa157d4a6c3ffc8730f377cc230070
Heavenstone <malolegendrelemaire@gmail.com>
2020-07-07 15:12:28 +0200

    back to previous test main.cpp

--- cli/main.cpp
+++ cli/main.cpp
@@ -5,7 +5,11 @@ using namespace brim;
 using namespace std;
 
 int main() {
-    Chunk* chunk = new Chunk();
+    Vec<u8> vec;
+
+    for (u8 i = 0; i <= 254; i++) {
+       vec.push(i);
+    }
 
     return 0;
 }
