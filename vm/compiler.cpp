d759e6d412e121b92ed79e09ee39465a597cb90f
Heavenstone <malolegendrelemaire@gmail.com>
2020-07-08 07:10:47 +0200

    added empty compiler system

--- vm/compiler.cpp
+++ vm/compiler.cpp
@@ -2,3 +2,25 @@
 #include "compiler.h"
 
 using namespace brim;
+
+struct State {
+    const Vec<Token> &tokens;
+    usize current;
+
+    Chunk *chunk;
+
+    const Token &current_token()const {
+        return tokens[current];
+    }
+};
+
+Chunk *compile(const Vec<Token> &tokens) {
+    State state{
+        tokens: tokens,
+        current: 0,
+        chunk: new Chunk()
+    };
+
+
+    return state.chunk;
+}
--- vm/compiler.h
+++ vm/compiler.h
@@ -1,5 +1,12 @@
 #pragma once
 
+#include "common.h"
 #include "chunk.h"
+#include "vec.h"
+#include "scanner.h"
 
-namespace brim {}
+namespace brim {
+
+    Chunk compile(const Vec<Token> &tokens);
+
+}
